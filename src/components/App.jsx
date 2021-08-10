34add6eb9201759f6929c242072ee2cd388b75ef
codymart1986 <codymart1986@hotmail.com>
2021-08-10 15:54:33 -0700

    completed navigation functionality for going to next and previous books

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,8 +12,29 @@ class App extends Component {
         ]
         this.state = {
             bookNumber: 1
+        }            
+    }
+
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
         }
-            
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length - 1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        })
     }
 
     render(){
@@ -22,7 +43,7 @@ class App extends Component {
                 <TitleBar />
                 <div className="row">
                     <div className="col-md-4">
-                        {/*Button here to move to the previous book viewed*/}
+                        <button onClick={this.goToPreviousBook}>Previous Book</button>
                     </div>
                     <div className="col-md-4">
                         {/*Display book with cover here*/}
@@ -30,7 +51,7 @@ class App extends Component {
                         <h4>{this.books[this.state.bookNumber].author}</h4>
                     </div>
                     <div className="col-md-4">
-                        {/*Button here to move to the previous book viewed*/}
+                        <button onClick={this.goToNextBook}>Next Book</button>
                     </div>
                 </div>
 
